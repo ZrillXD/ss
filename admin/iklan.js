e3bf4c35228405d9ed3899a715be771db23ff024
RetoBot <sadboyretor@gmail.com>
2022-04-08 21:58:19 +0700

    BOTZ23

---
+++ admin/iklan.js
@@ -0,0 +1,11 @@
+exports.iklan1 = () => {
+  return `[ OPEN TOP UP GAME FF ML AND PES ]`
+};
+
+exports.iklan2 = () => {
+  return `[ OPEN SEWA BOT WA ]`
+};
+
+exports.iklan3 = () => {
+  return `[ OPEN SEWA RUN BOT ]`
+};
